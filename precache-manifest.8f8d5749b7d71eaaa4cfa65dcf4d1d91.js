2f5babb6239d67d206608f1739c670f3734d3922
jenniferlai43 <jenniferlai43@gmail.com>
2019-10-29 00:33:49 -0700

    Updates

---
+++ precache-manifest.8f8d5749b7d71eaaa4cfa65dcf4d1d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465629e73a3e5b5c3c2f59b52713fbe6",
+    "url": "/cs291a_proj3_fe_pub/index.html"
+  },
+  {
+    "revision": "79e90c705746a88360e1",
+    "url": "/cs291a_proj3_fe_pub/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5e27ea75f3cfd731799e",
+    "url": "/cs291a_proj3_fe_pub/static/js/2.fb9b3ceb.chunk.js"
+  },
+  {
+    "revision": "79e90c705746a88360e1",
+    "url": "/cs291a_proj3_fe_pub/static/js/main.3608e336.chunk.js"
+  },
+  {
+    "revision": "b05a54c5e153868d3071",
+    "url": "/cs291a_proj3_fe_pub/static/js/runtime-main.8c1878a7.js"
+  }
+]);
